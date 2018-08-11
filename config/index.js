dffe01c46b6e5c6354ebc38a519dac92ac8568f9
许春洋 <xuchunyang@Peter-MBA.local>
2018-08-11 18:01:49 +0800

    更改服务名称

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'peterdev.app.goodiber.com', // can be overwritten by process.env.HOST
+    host: 'h5dev.iber2.com', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
